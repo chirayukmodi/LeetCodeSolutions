df2be78b2a115355dc39f44183164aad08058067
Chirayu <modichirayu897@gmail.com>
2017-06-17 17:16:05 +0530

    Reverse LinkList

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    //Utility Function
+    void reverseListUtil(ListNode **head, ListNode *curr, ListNode * prev)
+    {
+            if (!curr)
+                return;
+            reverseListUtil(head, curr->next, curr);
+            if (!curr->next)
+                *head = curr;
+            curr->next = prev;
+    }
+    ListNode* reverseList(ListNode* head) 
+    {
+        if (!head)
+            return NULL;
+        if (!(head->next))
+            return head;
+        reverseListUtil(&head, head, NULL);
+        return head;
+    }
+};
